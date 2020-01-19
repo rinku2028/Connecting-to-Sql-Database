012b62ca10d9b6023aa3f651b220f0a7a1442b4f
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-19 23:00:05 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Data Source=desktop-tb3r1mj;Initial Catalog=access;Integrated Security=True";
+            SqlConnection sqlconn = new SqlConnection(str);
+            try
+            {
+                sqlconn.Open();
+                Console.WriteLine("Database connected");
+            }
+            catch
+            {
+                Console.WriteLine("Database not connected");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
