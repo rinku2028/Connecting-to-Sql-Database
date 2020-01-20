c14c7644197adaaf9ffcfea2b87f55d8299bc735
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-21 00:25:02 +0545

    Add files via upload

---
+++ fetchdata.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace dataconn
+{
+    class fetchdata
+    {
+        static void Main(string[] args)
+        {
+
+            //fetching data from sql and displaying in console
+            string sql = "Data Source=desktop-tb3r1mj;Initial Catalog=access;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(sql);
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Connection opened");
+                SqlCommand sqldata = new SqlCommand("select * from info", conn);
+                sqldata.CommandType = CommandType.Text;
+                SqlDataReader dr = sqldata.ExecuteReader();
+                //  if (dr.Read())
+                while (dr.Read())
+                {
+                    Console.WriteLine(dr[0].ToString() + "\t" + dr[1].ToString());
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Database not connected");
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
